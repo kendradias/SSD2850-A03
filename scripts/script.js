5abcea19b2aff00890eda230af472ea546583b01
Kendra Dias <kendra.dias@hotmail.com>
2024-11-27 15:38:11 -0800

    added css styling and html for hamburger menu as well as simple js for active states

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// optional to use js, not a requirement.
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
